a1e170ce40273cbc3f1ccb7ea6b620c80b538f91
TheAditya-10 <aditya.pratap.singh.tomar.1082006@gmail.com>
2026-04-01 12:55:56 +0530

    updated

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,17 +1,17 @@
 export const EVENT_NAME = "JEC CSE '26 Farewell";
 export const EVENT_SUBTITLE =
   "Crafted with respect by your juniors, to celebrate your journey.";
-export const EVENT_DATE_LABEL = "4th or 6th April";
+export const EVENT_DATE_LABEL = "7th April";
 export const EVENT_VENUE_LABEL = "To Be Revealed Soon";
-export const CONTRIBUTION_AMOUNT = 1000;
+export const CONTRIBUTION_AMOUNT = 850;
 
 export function getNextFarewellDate(reference = new Date()) {
   const currentYear = reference.getFullYear();
-  const candidate = new Date(`${currentYear}-04-04T18:00:00+05:30`);
+  const candidate = new Date(`${currentYear}-04-07T18:00:00+05:30`);
 
   if (candidate.getTime() > reference.getTime()) {
     return candidate;
   }
 
-  return new Date(`${currentYear + 1}-04-04T18:00:00+05:30`);
+  return new Date(`${currentYear + 1}-04-07T18:00:00+05:30`);
 }
